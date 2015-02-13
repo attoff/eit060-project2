15ccb95a27a3e1fbd56348bb53033cd551e39e92
attoff <viktor.attoff@gmail.com>
2015-02-13 14:07:31 +0100

    test

--- src/Server/Journal.java
+++ src/Server/Journal.java
@@ -10,51 +10,51 @@ public class Journal {
 
     }
 
-//    /**
-//     * Method for adding an entry to the Journal, should only be accesible by a nurse or doctor.
-//     *
-//     * @
-//     */
-//    public addEntry(JournalEntry newEntry) {
-//        if (getWritePermit(newEntry.getUser) {
-//            list.add(newEntry);
-//        }else{
-//            System.out.println("You are not authorized to add entries to this journal");
-//        }
-//    }
-//
-//    public readEntry(/*something to specify entry?*/ User u) {
-//
-//    }
-//
-//    public deleteJournal(User u) {
-//    }
-//
-//    private getReadPermit(User currentUser) {
-//        if (currentUser.equals(patient)) {
-//            return true;
-//        } else if (currentUser.isStaff() && currentUser.getDivision() == patient.getDivision()) {
-//            return true;
-//        } else if (currentUser.isGovernment()) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-//
-//    private getWritePermit(User currentUser) {
-//        if (patient.isTreatedBy(currentUser) {
-//            return true;
-//        }else{
-//            return false;
-//        }
-//    }
-//
-//    private getDeletePermit(User currentUser) {
-//        if (currentUser.isGovernment()) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
+    /**
+     * Method for adding an entry to the Journal, should only be accesible by a nurse or doctor.
+     *
+     * @
+     */
+    public addEntry(JournalEntry newEntry) {
+        if (getWritePermit(newEntry.getUser())) {
+            list.add(newEntry);
+        } else {
+            System.out.println("You are not authorized to add entries to this journal");
+        }
+    }
+
+    public JournalEntry readEntry(/*something to specify entry?*/ User u) {
+        return null;
+    }
+
+    public void deleteJournal(User u) {
+    }
+
+    private boolean getReadPermit(User currentUser) {
+        if (currentUser.equals(patient)) {
+            return true;
+        } else if (currentUser.isStaff() && currentUser.getDivision() == patient.getDivision()) {
+            return true;
+        } else if (currentUser.isGovernment()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean getWritePermit(User currentUser) {
+        if (patient.isTreatedBy(currentUser) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private boolean sgetDeletePermit(User currentUser) {
+        if (currentUser.isGovernment()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
